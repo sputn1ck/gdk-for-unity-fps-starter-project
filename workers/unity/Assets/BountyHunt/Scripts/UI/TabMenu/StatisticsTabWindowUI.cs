97ff2d2c39b71ad05560461ac1992278d2fe446e
Marlin Küppers <marlin@fabervisio.com>
2020-03-05 19:57:57 +0100

    adding Statistics Panel

---
+++ workers/unity/Assets/BountyHunt/Scripts/UI/TabMenu/StatisticsTabWindowUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StatisticsTabWindowUI : TabMenuWindowUI
+{
+
+    //player
+    public TMP_InputField balanceText;
+    public TMP_InputField currentEarningsText;
+    public TMP_InputField currentBountyText;
+    public TMP_InputField currentKillsAndDeathsText;
+    public TMP_InputField lifeTimeKillsAndDeathsText;
+    public TMP_InputField lifeTimeEarningsText;
+
+    //game
+    public TMP_InputField currentPotText;
+    public TMP_InputField currentBountyOnPlayersText;
+    public TMP_InputField currentLootOnMapText;
+    public TMP_InputField totalGameSatsText;
+    public TMP_InputField totalPlayersLifeTimeKillsText;
+    public TMP_InputField allTimeMostKillsText;
+    public TMP_InputField totalPlayersLifetimeDeathsText;
+    public TMP_InputField allTimeMostDeathsText;
+    public TMP_InputField TotalPlayersLifetimeEarnings;
+    public TMP_InputField AllTimeMostEarnings;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    
+}
