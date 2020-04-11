c0fa0ed027cf1c3a315a0fb92eb4f79d3c4de53d
Marlin Küppers <marlin@fabervisio.com>
2020-04-12 00:14:01 +0200

    buttonsettings
    + resoloution example

--- workers/unity/Assets/BountyHunt/Scripts/UI/MainMenu/GraphicSettingsMenuUI.cs
+++ workers/unity/Assets/BountyHunt/Scripts/UI/MainMenu/GraphicSettingsMenuUI.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
 
 public class GraphicSettingsMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button resoloutionButton;
+    public List<Vector2Int> resoloutions;
+
+    private void Start()
     {
-        
+        resoloutionButton.onClick.AddListener(OnResoloutionButtonClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnResoloutionButtonClick()
     {
-        
+        List<LabelAndAction> resos = new List<LabelAndAction>();
+        foreach(Vector2Int res in resoloutions)
+        {
+            resos.Add(resoloutionLabelAndAction(res));
+        }
+        PopUpEventArgs args = new PopUpEventArgs("Resoloution", "", resos, true);
+        ClientEvents.instance.onPopUp.Invoke(args);
     }
+
+    LabelAndAction resoloutionLabelAndAction(Vector2Int res)
+    {
+        string label = res.x + " x " + res.y;
+        UnityAction action = delegate { setResoloution(res); };
+        return new LabelAndAction(label, action);
+    }
+
+    void setResoloution(Vector2Int res)
+    {
+        resoloutionButton.GetComponentInChildren<TextMeshProUGUI>().text = res.x + " x " + res.y;
+    }
+
 }
