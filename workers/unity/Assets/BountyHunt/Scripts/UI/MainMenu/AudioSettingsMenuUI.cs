eb323b43f516b7fbf9ddde1c7be586da1efe2f86
Marlin Küppers <marlin@fabervisio.com>
2020-04-14 23:31:41 +0200

    Audio settings

--- workers/unity/Assets/BountyHunt/Scripts/UI/MainMenu/AudioSettingsMenuUI.cs
+++ workers/unity/Assets/BountyHunt/Scripts/UI/MainMenu/AudioSettingsMenuUI.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioSettingsMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider masterVolumeSlider;
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
+    public SlideSubMenuUI subMenu;
     void Start()
     {
-        
+        masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
+        musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxVolumeSlider.onValueChanged.AddListener(SetSfxVolume);
+        Refresh();
+        subMenu.onActivate.AddListener(Refresh);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Refresh()
     {
-        
+        masterVolumeSlider.value = AudioManager.instance.GetVolume(VolumeType.MASTER);
+        musicVolumeSlider.value = AudioManager.instance.GetVolume(VolumeType.MUSIC);
+        sfxVolumeSlider.value = AudioManager.instance.GetVolume(VolumeType.SFX);
+    }
+
+    private void SetMasterVolume(float vol)
+    {
+        AudioManager.instance.SetVolume(VolumeType.MASTER, vol);
+    }
+
+    private void SetMusicVolume(float vol)
+    {
+        AudioManager.instance.SetVolume(VolumeType.MUSIC, vol);
+    }
+
+    private void SetSfxVolume(float vol)
+    {
+        AudioManager.instance.SetVolume(VolumeType.SFX, vol);
     }
 }
