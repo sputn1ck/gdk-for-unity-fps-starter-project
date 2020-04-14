aa53a6c53d813c8515a0ea7f148f04a5c0d25990
Marlin Küppers <marlin@fabervisio.com>
2020-04-14 19:36:25 +0200

    Graphic settings

--- workers/unity/Assets/BountyHunt/Scripts/UI/MainMenu/GraphicSettingsMenuUI.cs
+++ workers/unity/Assets/BountyHunt/Scripts/UI/MainMenu/GraphicSettingsMenuUI.cs
@@ -4,38 +4,117 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class GraphicSettingsMenuUI : MonoBehaviour
 {
-    public Button resoloutionButton;
-    public List<Vector2Int> resoloutions;
+    public Button resolutionButton;
+
+    public Button qualityButton;
+
+    public Button displayModeButton;
 
     private void Start()
     {
-        resoloutionButton.onClick.AddListener(OnResoloutionButtonClick);
+        resolutionButton.onClick.AddListener(OnResolutionButtonClick);
+        qualityButton.onClick.AddListener(OnQualityButtonClick);
+        displayModeButton.onClick.AddListener(OnDisplayModeButtonClick);
+
+        resolutionButton.GetComponentInChildren<TextMeshProUGUI>().text = Screen.currentResolution.width + " x " + Screen.currentResolution.height;
+        qualityButton.GetComponentInChildren<TextMeshProUGUI>().text = ((GraphicsQuality)QualitySettings.GetQualityLevel()).ToString();
+        displayModeButton.GetComponentInChildren<TextMeshProUGUI>().text = ((DisplayMode)Screen.fullScreenMode).ToString();
+
+        resolutionButton.GetComponentInChildren<TextSizer>().Refresh();
+        qualityButton.GetComponentInChildren<TextSizer>().Refresh();
+        displayModeButton.GetComponentInChildren<TextSizer>().Refresh();
+
+
     }
 
-    void OnResoloutionButtonClick()
+    //resolution
+    void OnResolutionButtonClick()
     {
         List<LabelAndAction> resos = new List<LabelAndAction>();
-        foreach(Vector2Int res in resoloutions)
+        foreach(Resolution res in Screen.resolutions)
         {
-            resos.Add(resoloutionLabelAndAction(res));
+            resos.Add(resolutionLabelAndAction(res));
         }
-        PopUpEventArgs args = new PopUpEventArgs("Resoloution", "", resos, true);
+        PopUpEventArgs args = new PopUpEventArgs("Resolution", "", resos, true);
         ClientEvents.instance.onPopUp.Invoke(args);
     }
 
-    LabelAndAction resoloutionLabelAndAction(Vector2Int res)
+    LabelAndAction resolutionLabelAndAction(Resolution res)
     {
-        string label = res.x + " x " + res.y;
-        UnityAction action = delegate { setResoloution(res); };
+        string label = res.width + " x " + res.height;
+        UnityAction action = delegate { SetResolution(res); };
         return new LabelAndAction(label, action);
     }
 
-    void setResoloution(Vector2Int res)
+    void SetResolution(Resolution res)
     {
-        resoloutionButton.GetComponentInChildren<TextMeshProUGUI>().text = res.x + " x " + res.y;
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+        resolutionButton.GetComponentInChildren<TextMeshProUGUI>().text = res.width + " x " + res.height;
+        resolutionButton.GetComponentInChildren<TextSizer>().Refresh();
     }
+
+    //quality
+    void OnQualityButtonClick()
+    {
+        List<LabelAndAction> quals = new List<LabelAndAction>();
+        foreach (GraphicsQuality qual in (GraphicsQuality[])Enum.GetValues(typeof(GraphicsQuality)))
+        {
+            quals.Add(QualityLabelAndAction(qual));
+        }
+        PopUpEventArgs args = new PopUpEventArgs("Quality", "", quals, true);
+        ClientEvents.instance.onPopUp.Invoke(args);
+    }
+    void SetQuality(GraphicsQuality quality)
+    {
+        QualitySettings.SetQualityLevel((int)quality);
+        qualityButton.GetComponentInChildren<TextMeshProUGUI>().text = quality.ToString();
+        qualityButton.GetComponentInChildren<TextSizer>().Refresh();
+    }
+    public enum GraphicsQuality
+    {
+        low = 0,
+        medium = 1,
+        high = 2
+    }
+    LabelAndAction QualityLabelAndAction(GraphicsQuality quality)
+    {
+        string label = quality.ToString();
+        UnityAction action = delegate { SetQuality(quality); };
+        return new LabelAndAction(label, action);
+    }
+
+    //display mode
+    void OnDisplayModeButtonClick()
+    {
+        List<LabelAndAction> modes = new List<LabelAndAction>();
+        foreach (DisplayMode mode in (DisplayMode[])Enum.GetValues(typeof(DisplayMode)))
+        {
+            modes.Add(DisplayModeLabelAndAction(mode));
+        }
+        PopUpEventArgs args = new PopUpEventArgs("Display Mode", "", modes, true);
+        ClientEvents.instance.onPopUp.Invoke(args);
+    }
+    void SetDisplayMode(DisplayMode mode)
+    {
+        Screen.fullScreenMode = (FullScreenMode)mode;
+        displayModeButton.GetComponentInChildren<TextMeshProUGUI>().text = mode.ToString();
+        displayModeButton.GetComponentInChildren<TextSizer>().Refresh();
+    }
+    public enum DisplayMode
+    {
+        fullscreen = 1,
+        windowed = 3
+    }
+    LabelAndAction DisplayModeLabelAndAction(DisplayMode mode)
+    {
+        string label = mode.ToString();
+        UnityAction action = delegate { SetDisplayMode(mode); };
+        return new LabelAndAction(label, action);
+    }
+
 
 }
