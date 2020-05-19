9cd7dfe3e3fe22d0dae208587f72de09d2326d53
kon <kon@kon.ninja>
2020-05-19 10:24:55 +0200

    fix ragdoll skin change
    closes #160

---
+++ workers/unity/Assets/BountyHunt/Scripts/Game/Skins/SkinChanger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinChanger : MonoBehaviour
+{
+    [SerializeField] private Transform RendererParent;
+    private Renderer[] renderers;
+
+    public void Awake()
+    {
+        renderers = RendererParent.GetComponentsInChildren<SkinnedMeshRenderer>();
+    }
+    
+
+    public void UpdateSkin(string skinId)
+    {
+        var skin = SkinsLibrary.MasterInstance.GetSkin(skinId);
+        if (skin.material != null)
+        {
+            foreach (var renderer in renderers)
+            {
+                renderer.material = skin.material;
+            }
+        }
+    }
+}
