754818fa7b4b9fb9ec97b373983e5fc6c67338f7
Marlin Küppers <marlin@fabervisio.com>
2020-06-09 15:26:44 +0200

    fading sponsor Info

--- workers/unity/Assets/BountyHunt/Scripts/UI/SponsorTileUI.cs
+++ workers/unity/Assets/BountyHunt/Scripts/UI/SponsorTileUI.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class SponsorTileUI : MonoBehaviour
+public class SponsorTileUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI satsText;
@@ -12,12 +13,27 @@ public class SponsorTileUI : MonoBehaviour
     public Button openLaterButton;
     public TextMeshProUGUI savedForLaterText;
 
+    Animator animator;
     Advertiser advertiser;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Set(Advertiser advertiser)
     {
         advertiser = advertiser;
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animator.SetBool("showInfo", true);
+    }
 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        animator.SetBool("showInfo", false);
+
+    }
 }
